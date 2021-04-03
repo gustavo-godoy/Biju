7c04aa68b69e32d8599deaf132fa0f384d2949c8
Gustavo Godoy <contato@godoy.dev.br>
2021-04-02 22:57:51 -0300

    Simplificar Promise
    Agora a função recebe um número variável de
    argumentos (pelo menos 1), no qual o último
    deve ser sempre uma função de callback.
    Assim não precisa usar toda a interface
    de Promise, simplificando pro usuário final
    (e ao mesmo tempo limitando-o).
    
    Porém Biju é opionada mesmo, então acho
    que tudo certo simplificar assim.

--- public/js/serverCall.js
+++ public/js/serverCall.js
@@ -3,20 +3,31 @@
 callHandler = {
     caller: function(service) {
         return function _caller(...args){
-        
+
+            // Argumentos       
+            if (args.length < 1) {
+                throw "Exception: Pelo menos um argumento (callback) é necessário."
+            }
+
+            callback = args.pop()
+            
+            if ( !(callback instanceof Function) ){
+                throw "Exception: Ultimo argumento precisa ser uma função para callback"
+            }
+
+            // Função auxíliar para JSON.stringify
             function stringifyReplacer(key, value) {
                 if (value == null || value == ""){
                     return undefined;
                 }
                 return value;
-            }            
-            
+            }
+
             response = fetch('/run/'+service, {
                 method: 'POST',
                 headers: {'Content-Type': 'application/json'},
                 body: JSON.stringify({'_args': args}, stringifyReplacer)
-            });
-            
+            }).then(response => response.json()).then(callback)
 
             return response;
         }
